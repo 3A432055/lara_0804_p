daefae4da0111bc35b8110c7fa47112114beb5aa
CHEN <david860120@gmail.com>
2019-10-18 19:59:14 +0800

    修改最終版本

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-
+require __DIR__.'/bootstrap.php';
 require __DIR__.'/vendor/autoload.php';
-
+// connect to dabase
 use Carbon\Carbon;
 Carbon::setLocale('zh-tw');
-
-// connect to dabase
 try {
     $dsn = 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE.';charset='.DB_CHARSET;
     $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
@@ -13,7 +11,6 @@ try {
     echo  "Error: ".$e->getMessage()."＜br/＞";
     die();
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,63 +34,63 @@ try {
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
 </head>
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Laravel 道場</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="http://www.laravel-dojo.com/about/dojo" target="_blank">關於道場</a>
-                    </li>
-                    <li>
-                        <a href="http://www.laravel-dojo.com/workshops" target="_blank">工作坊</a>
-                    </li>
-                    <li>
-                        <a href="http://www.laravel-dojo.com/contact" target="_blank">聯絡我們</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
-
-        <!-- Page Header -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">文章清單
-                    <small><?php echo Carbon::now()->toDateString(); ?></small>
-                </h1>
-            </div>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Laravel 道場</a>
         </div>
-        <!-- /.row -->
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="http://www.laravel-dojo.com/about/dojo" target="_blank">關於道場</a>
+                </li>
+                <li>
+                    <a href="http://www.laravel-dojo.com/workshops" target="_blank">工作坊</a>
+                </li>
+                <li>
+                    <a href="http://www.laravel-dojo.com/contact" target="_blank">聯絡我們</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
 
-        <!-- Projects Row -->
-        <div class="row">
-            <?php $statement = $pdo->query("SELECT * FROM `posts` ORDER BY `created_at` DESC LIMIT 3"); ?>
-            <?php while($row = $statement->fetch(PDO::FETCH_OBJ)): ?>
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Page Header -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">文章清單
+                <small><?php echo Carbon::now()->toDateString(); ?></small>
+            </h1>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Projects Row -->
+    <div class="row">
+        <?php $statement = $pdo->query("SELECT * FROM `posts` ORDER BY `created_at` DESC LIMIT 3"); ?>
+        <?php while($row = $statement->fetch(PDO::FETCH_OBJ)): ?>
             <div class="col-md-4 portfolio-item">
                 <a href="#">
                     <img class="img-responsive" src="http://placehold.it/700x400" alt="">
@@ -107,30 +104,30 @@ try {
                     發表於 <?=Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->diffForHumans()?>
                 </p>
             </div>
-            <?php endwhile; ?>
+        <?php endwhile; ?>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Laravel 道場訓練教材  2015</p>
+            </div>
         </div>
         <!-- /.row -->
+    </footer>
 
-        <hr>
+</div>
+<!-- /.container -->
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Laravel 道場訓練教材  2015</p>
-                </div>
-            </div>
-            <!-- /.row -->
-        </footer>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
 
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
 
 </body>
 

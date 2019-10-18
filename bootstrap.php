6511c0adac05da60e526613dfc7db3952ec1e59f
CHEN <david860120@gmail.com>
2019-10-18 19:02:40 +0800

    git init commit & DB setting

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 33060);
+define('DB_DATABASE', 'lara_0804_p');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
